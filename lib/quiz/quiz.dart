00ab2cd12541002cd00c2d4c1b5bde84c1968aa8
Ansh Saini <anshsaini625@gmail.com>
2022-05-22 17:21:09 +0530

    setup quiz state

--- lib/quiz/quiz.dart
+++ lib/quiz/quiz.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:quizapp/quiz/quiz_state.dart';
+import 'package:quizapp/services/firestore.dart';
+import 'package:quizapp/services/models.dart';
+import 'package:quizapp/shared/loading.dart';
 
-class Quiz extends StatelessWidget {
-  const Quiz({Key? key}) : super(key: key);
+class QuizScreen extends StatelessWidget {
+  const QuizScreen({Key? key, required this.quizId}) : super(key: key);
+  final String quizId;
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Quiz");
+    return ChangeNotifierProvider(
+      create: (_) => QuizState(),
+      child: FutureBuilder<Quiz>(
+        future: FirestoreService().getQuiz(quizId),
+        builder: (context, snapshot) {
+          final state = Provider.of<QuizState>(context);
+
+          if (!snapshot.hasData || snapshot.hasError) {
+            return const Loader();
+          } else {
+            final quiz = snapshot.data!;
+
+            return const Scaffold();
+          }
+        },
+      ),
+    );
   }
 }
